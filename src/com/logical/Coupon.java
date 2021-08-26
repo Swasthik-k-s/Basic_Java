3bc423db4e49bfb331093e6e8e2ddae2a967d127
Swasthik <swasthik.ks22@gmail.com>
2021-08-27 00:11:55 +0530

    Several incompleted programs were updated

--- src/com/logical/Coupon.java
+++ src/com/logical/Coupon.java
@@ -3,11 +3,50 @@ package com.logical;
 import java.util.*;
 
 public class Coupon {
+	public static int n;
+	public static ArrayList<Integer> arrayList=new ArrayList<>(); 
+	public static Random random = new Random();
+	
 	public static void coupon() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value of n");
+		n = sc.nextInt();
+		System.out.println("Enter the " + n + " distinct coupon numbers");
 
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter the Distinct Coupon Number");
-		int num = scanner.nextInt();
-		scanner.close();
+		// add the distinct coupon no. to dis_arr
+		for (int i = 0; i < n; i++) {
+			arrayList.add(sc.nextInt());
+		}
+		int count = 0;
+		while (true) {
+			count++;
+			int x = randomNum();
+			boolean ps = process(x);
+			if (ps)
+			{
+				System.out.println("Distinct coupon generated: "+x);
+				break;
+			}
+			
+		}
+
+		System.out.println("Number of random numbers needed to generate distinct coupon number is " +count);
+		sc.close();
 	}
+	
+	public static int  randomNum()
+    {
+    	return random.nextInt(10);
+    }
+	
+	public static boolean process(int x)
+    {
+    	int index = arrayList.indexOf(x);
+    	if(index != -1)
+    	{
+    		arrayList.remove(index);
+    		return true;
+    	}
+    	else return false;
+    }
 }
--- src/com/logical/StopWatch.java
+++ src/com/logical/StopWatch.java
@@ -1,7 +1,37 @@
 package com.logical;
 
+import java.util.*;
+
 public class StopWatch {
-	public static void stopWatch() {
-		
-	}
+		static double start = 0, stop = 0;
+
+		static void startTime() {
+			start = System.currentTimeMillis();
+			System.out.println("started at " + start);
+		}
+
+
+		static void stopTime() {
+			stop = System.currentTimeMillis();
+			System.out.println("stopped at " + stop);
+			elapsedTime();
+		}
+
+
+		static void elapsedTime() {
+			System.out.println("elapsed time is " + (stop - start) / 1000);
+		}
+
+		public static void stopWatch() {
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("enter 1 to start");
+			int startkey=scanner.nextInt();
+			if(startkey==1)
+				startTime();
+			System.out.println("enter 2 to stop");
+			int stopkey=scanner.nextInt();
+			scanner.close();
+			if(stopkey==2)
+				stopTime();
+		}
 }
